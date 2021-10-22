Fixed Random Rotation (bad angle, function does it counter clock-wise)

ParasiticEggDataset.py
@@ -1,6 +1,7 @@
 import os
-import torch
+import cv2
 import json
+import torch
 
 from PIL import Image
 from torchvision.ops import box_convert
@@ -27,7 +28,8 @@ class ParasiticEggDataset(torch.utils.data.Dataset):
   
   def __getitem__(self, idx):
     images = Image.open(self.inputs[idx]).convert("RGB")
-    print(images.size)
+    #images = cv2.cvtColor(cv2.imread(self.inputs[idx]), cv2.COLOR_BGR2RGB)
+    #print(images.shape)
     boxes = self.targets['boxes'][idx].copy()
     boxes = torch.as_tensor(boxes, dtype=torch.float32)
     if self.convert_boxes_coordinates:

main.py
@@ -260,15 +260,16 @@ def test(settings):
 					idxs = [np.random.randint(0,len(eggs_dataset_test))]
 				if show_predictions:
 					for idx in idxs:
-						prediction = model([eggs_dataset_test[idx][0].to('cuda')])
+						image, target = eggs_dataset_test[idx]
+						prediction = model([image.to('cuda')])
 						boxes = prediction[0]['boxes']
 						scores = prediction[0]['scores']
 						labels = prediction[0]['labels']
 						keep = torchvision.ops.nms(boxes, scores, 0.5)
 						new_outputs = keep_outputs(prediction[0], keep, remove_scores = remove_scores)
 						#print(idx, labels, scores)
-						img = draw_boxes(eggs_dataset_test[idx][0].permute(2,1,0).numpy().copy(), boxes, labels,scores)
-						img = draw_boxes(img, eggs_dataset_test[idx][1]['boxes'],eggs_dataset_test[idx][1]['labels'])
+						img = draw_boxes(image.permute(2,1,0).numpy().copy(), boxes, labels,scores)
+						img = draw_boxes(img, target['boxes'], target['labels'])
 
 						fname = os.path.join(output_path, 'test_%d.%d_nms.png' % (fold, idx))
 						fig, axs = plt.subplots(figsize=(20,20))

references/transforms.py
@@ -76,13 +76,11 @@ class ToTensor(nn.Module):
     def forward(self, image: Tensor,
                 target: Optional[Dict[str, Tensor]] = None) -> Tuple[Tensor, Optional[Dict[str, Tensor]]]:
         image = F.to_tensor(image)
-        #print(image.shape)
         return image, target
 
     def __call__(self, image: Tensor,
                 target: Optional[Dict[str, Tensor]] = None) -> Tuple[Tensor, Optional[Dict[str, Tensor]]]:
         image = F.to_tensor(image)
-        #print(image.shape)
         return image, target
 
 class Normalize(nn.Module):
@@ -314,16 +312,15 @@ class RandomRotation(nn.Module):
 
     def __call__(self, image: Tensor,
                 target: Optional[Dict[str, Tensor]] = None) -> Tuple[Tensor, Optional[Dict[str, Tensor]]]:
-        if torch.rand(1) < self.p:
+        #if torch.rand(1) < self.p:
+        if True:
             #print("IN!")
             angle = float(torch.empty(1).uniform_(float(-180), float(180)).item())
             image = F.rotate(image, angle)
             if target is not None:
                 width, height = F._get_image_size(image)
-
                 target["boxes"][:, [0, 2]] = target["boxes"][:, [2, 0]] - width/2
                 target["boxes"][:, [1, 3]] = target["boxes"][:, [1, 3]] - height/2
-
                 for i, box in enumerate(target["boxes"]):
                     #print(box)
                     new_box = self.get_new_box(box, angle)
@@ -340,7 +337,7 @@ class RandomRotation(nn.Module):
     def get_new_coordinate(self, old_coordinate, angle):
         radius = (old_coordinate[0]**2 + old_coordinate[1]**2) ** 0.5
         old_angle = np.arctan2(old_coordinate[1], old_coordinate[0])
-        new_angle = old_angle + np.pi*angle/180
+        new_angle = old_angle - np.pi*angle/180
         new_x = np.cos(new_angle)*radius
         new_y = np.sin(new_angle)*radius
         return [new_x, new_y]
@@ -390,12 +387,12 @@ class MotionBlur(nn.Module):
             kernel = np.zeros((kernel_size, kernel_size))
             kernel[int((kernel_size - 1) / 2), :] = np.ones(kernel_size)
             kernel = kernel/kernel_size
-            image = F.to_tensor(cv2.filter2D(image.permute(2,1,0).numpy(), -1, kernel))
+            image = F.to_tensor(cv2.filter2D(image.permute(1,2,0).numpy(), -1, kernel))
         elif torch.rand(1) < self.p:
             #print("vertical")
             kernel = np.zeros((kernel_size, kernel_size))
             kernel[:, int((kernel_size - 1) / 2)] = np.ones(kernel_size)
             kernel = kernel/kernel_size
-            image = F.to_tensor(cv2.filter2D(image.permute(2,1,0).numpy(), -1, kernel))
+            image = F.to_tensor(cv2.filter2D(image.permute(1,2,0).numpy(), -1, kernel))
         return image, target
 